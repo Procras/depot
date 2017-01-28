32c980261562ef683cc12793de8a6bccbe4a469b
Giancarlo <gfabrizi@gmail.com>
2017-01-28 17:45:08 +0100

    formats price to $

--- spec/features/store_features_spec.rb
+++ spec/features/store_features_spec.rb
@@ -8,4 +8,11 @@ feature 'store' do
       expect(page).to have_content 'Iphone'
     end
   end
+  context 'price' do
+    scenario 'shows properly formatted price' do
+      Product.create title: 'Iphone', description: 'expensive', id: 1, image_url: "https://i.ytimg.com/vi/LA2Cgan9j5I/maxresdefault.jpg", price: 98.99
+      visit 'store/index'
+      expect(page).to have_content('$98.99')
+    end
+  end
 end
